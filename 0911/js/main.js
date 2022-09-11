86bbd8326437aaca43e881a6ba28a7659e797b03
DC-hashimoto-naoki <hashimoto_naoki@discovery-inc.com>
2022-09-11 15:24:22 +0900

    temp 0911

---
+++ 0911/js/main.js
@@ -0,0 +1,94 @@
+
+$(function () {
+    var jsTargets = $('.js-target');
+
+    function callback() {
+        $(jsTargets).each(function () {
+            var offset = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+
+            // when scrolling 40% of windows' height
+            var paddingTop = windowHeight * 0.4;
+            if (scroll > offset - windowHeight + paddingTop) {
+                if ($(this).hasClass("concept-texts")) { fadeInAnimationOnConceptText($(this)); }
+                $(this).addClass("show");
+                if ($(this).hasClass("cosmetic-image")) { removeTransittion($(this)); }
+            }
+        })
+    }
+
+    function fadeInAnimationOnConceptText(element) {
+        var children = element.children();
+        children.each((index, child) => {
+            setTimeout(() => {
+                child.classList.add('show')
+            }, index * 300)
+        })
+    }
+
+    function removeTransittion(element) {
+        setTimeout(() => {
+            element.addClass('transition-0');
+        }, 700)
+    }
+
+    $(window).on('scroll', callback);
+
+    // fade in animation
+    $('.js-fadeIn').hide();
+    $('.js-fadeIn').delay(1000).fadeIn();
+
+    $('.js-slideIn').addClass('show');
+
+    $('.js-fadeIn-scroll').hide();
+    $('.js-fadeIn-scroll').delay(1500).fadeIn();
+    $(window).on('scroll', () => {
+        $('.js-fadeIn-scroll').fadeOut(300);
+    })
+
+
+
+    // parallax animation in cosmetic section
+    var cosmeticImages = $('.cosmetic-image');
+    $(window).on('scroll', () => {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+
+        $(cosmeticImages).each(function () {
+            var offsetTop = $(this).offset().top;
+            var objectHeight = $(this).height();
+
+            if (scroll > offsetTop - windowHeight &&
+                scroll < offsetTop + objectHeight) {
+                var min = (offsetTop - windowHeight);
+                var max = (offsetTop + objectHeight);
+                var percent = (scroll - min) / (max - min) * 100;
+                $(this).css('background-position-y', percent + '%')
+            }
+        })
+    })
+
+    // parallax animation in product section
+    // $(window).on('scroll', () => {
+    //     var windowHeight = $(window).height();
+    //     var scroll = $(window).scrollTop();
+
+    //     var productOffsetTop = $('.product').offset().top;
+    //     var cosmeticOffsetTop = $('.cosmetic').offset().top;
+    //     var cosmeticHeight = $('.cosmetic').height();
+    //     var cosmeticBottom = cosmeticOffsetTop + cosmeticHeight;
+    //     var margin = 500;
+    //     $('.product').css('top', windowHeight + 'px');
+
+    //     var diff = windowHeight + scroll - cosmeticBottom;
+    //     // $('.product').css('top', cosmeticBottom - diff + 'px');
+
+    //     if (scroll > cosmeticBottom - windowHeight) {
+    //         console.log("active");
+    //         var scrollVolume = scroll - (cosmeticBottom - windowHeight)
+    //         $('.product').css('top', windowHeight - scrollVolume * 2 + margin + 'px');
+    //     }
+
+    // })
+});
---
+++ 0911/js/scrollTrigger.js
@@ -0,0 +1,30 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+    ScrollTrigger.create({
+        trigger: "#qeta-main",
+        start: "bottom 100%",
+        pin: "#qeta-main",
+        pinSpacing: false,
+        onEnter: () => fixedFooter(),
+        onEnterBack: () => freeFooter(),
+    });
+    
+    ScrollTrigger.create({
+        trigger: "#js-ParallaxContents",
+        start: "top 100%",
+        end: "bottom 0%",
+        pinSpacing: false,
+        onLeave: () => freeFooter(),
+        onEnterBack: () => fixedFooter(),
+    });
+    
+    function fixedFooter() {
+        let windowHeight = window.outerHeight;
+        document.getElementById('js-fixedFooter').classList.add('fixedFooter--fixed');
+        document.getElementById('js-ParallaxContents').style.marginBottom = windowHeight + 'px';
+    }
+    
+    function freeFooter() {
+        document.getElementById('js-fixedFooter').classList.remove('fixedFooter--fixed');
+        document.getElementById('js-ParallaxContents').style.marginBottom = '0';
+    }
+})
